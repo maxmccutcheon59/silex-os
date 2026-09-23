4eddb5e22dcd3ac10f9cafb7b6897b5aec25bf06
maxmccutcheon59 <maxmccutcheon59@gmail.com>
2026-09-22 22:50:49 -0400

    Remove loop.py compatibility shim

--- src/silex/loop.py
+++
@@ -1,9 +0,0 @@
-"""Back-compat shim. Prefer silex.wedge."""
-
-from silex.wedge import KITTING_STEPS as STEPS
-from silex.wedge import build_kitting_cell
-
-
-def build_demo_world():
-    graph, ledger, runtime, issuer, _actor = build_kitting_cell()
-    return graph, ledger, runtime, issuer
